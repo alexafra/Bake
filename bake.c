a1b72b4839d296dc7aa0bcaf046a2fe85ff135c8
JacobU <kieren.underwood@gmail.com>
2018-10-11 18:50:15 +0800

    next

--- bake.c
+++ bake.c
@@ -2,7 +2,9 @@
 
 //  Written by Chris.McDonald@uwa.edu.au, September 2018
 
+#if defined(__linux__)
 
+#endif
 
 
 //  DEVELOPED IN LECTURE 9  (but here using pointers)
@@ -13,21 +15,30 @@
 int main(int argc, char *argv[])
 {
     if(argc > 1) {
-        FILE *fp        = fopen(argv[1], "r");
+        FILE *fp1        = fopen(argv[1], "r");
 
-        if(fp == NULL) {
+        if(fp1 == NULL) {
             perror(argv[1]);
             return 1;
         }
-        while(!feof(fp)) {
-            char *line = nextline(fp);  // HANDLES CONTINUATION LINES
+        
+        File *fp2 = fopen("tempbake.txt","w");
+        if(fp2 == NULL)
+        {
+            perror()
+        }
+        
+        readandwrite(fp1, fp2);
+         
+        while(!feof(fp1)) {
+            char *line = nextline(fp1);  // HANDLES CONTINUATION LINES
 
             if(line) {
                 printf("%8s()\t%s\n", __func__, line);
                 free(line);
             }
         }
-        fclose(fp);                     // WE OPENED IT, SO WE CLOSE IT
+        fclose(fp1);                     // WE OPENED IT, SO WE CLOSE IT
     }
     return 0;
 }

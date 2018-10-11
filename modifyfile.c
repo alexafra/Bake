a6b707e74790d975359b68f42b20409ae5679542
Alexander Frazis <alexanderfrazis@gmail.com>
2018-10-11 19:38:12 +0800

    nuch ado about nothin

--- modifyfile.c
+++ modifyfile.c
@@ -1,6 +1,14 @@
 #include "bake.h"
 
-modifyfile(File **fp1, File **fp2)
+modifyfile(File *fp1, File *fp2)
 {
-    
+    while(!feof(fp1)) {
+        char *line = nextline(fp1);  // HANDLES CONTINUATION LINES
+
+        if(line) {
+            printf("%8s()\t%s\n", __func__, line);
+            free(line);
+        }
+  
+    }
 }

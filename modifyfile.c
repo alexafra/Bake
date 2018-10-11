25a4b58b0df91850bbbc05a286fdee4e4cdb8329
Alexander Frazis <alexanderfrazis@gmail.com>
2018-10-11 20:07:07 +0800

    so far

--- modifyfile.c
+++ modifyfile.c
@@ -4,7 +4,17 @@ modifyfile(File *fp1, File *fp2)
 {
     while(!feof(fp1)) {
         char *line = nextline(fp1);  // HANDLES CONTINUATION LINES
-        //if 
+        
+        //what does return and when.
+        //null means end if file
+        //Otherwise is a pointer that is a string
+
+        if (line) {
+        	/*
+				reset loop when hash is found.
+
+        	*/
+        }
 
 
         if(line) {

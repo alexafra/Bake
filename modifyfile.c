e58646d4ce5222dea8396e6b8b71b43c744bf08f
Alexander Frazis <alexanderfrazis@gmail.com>
2018-10-11 19:40:13 +0800

    lala

--- modifyfile.c
+++ modifyfile.c
@@ -4,6 +4,8 @@ modifyfile(File *fp1, File *fp2)
 {
     while(!feof(fp1)) {
         char *line = nextline(fp1);  // HANDLES CONTINUATION LINES
+        //if 
+
 
         if(line) {
             printf("%8s()\t%s\n", __func__, line);

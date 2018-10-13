645f6bb49c0f6f8b5fcd448933b7bea317d2efcd
JacobU <kieren.underwood@gmail.com>
2018-10-13 19:26:24 +0800

    var

---
+++ expand_variables.c
@@ -0,0 +1,15 @@
+#include "bake.h"
+
+expand_variables(char *line) {
+	
+	//search until we find $()
+	//temporarily store the string between ()
+	//if find that... then search our global array for variable substition
+	//if it finds it, then substitute it it
+	//if not, check for special cases... and substitute in
+	//if not, check for environmental process subsitutions, and use these
+
+	//if all these fail, enter the empty string
+
+	//return to the process_bakefile function 	
+}

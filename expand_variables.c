c2d5d12fc915f86aeda3c164426eb7082ea29f3c
JacobU <kieren.underwood@gmail.com>
2018-10-13 20:22:14 +0800

    variable exp update

--- expand_variables.c
+++ expand_variables.c
@@ -3,12 +3,34 @@
 expand_variables(char *line) {
 	
 	int i = 0;
-
+	int j = 0;
+	//Search until you find $(
 	while(line[i] != '$' && line[i+1] != '(') {
+		if(line[i] == '\0') {
+			return;
+		}
 		i++;
 	}
+	i = i + 2;
 
-	
+	char *name;
+	while(line[i] != ')') {
+		name[j] = line[i];
+	}
+
+	do something here about exchanging the name
+
+	i = 0;
+
+	while (line[i] != '$' && line[i+1] != '(') {
+		if(line[i] == '\0') {
+			return;
+		}
+		i++
+	}
+	free(name);
+	expand_variables();
+
 	//search until we find $()
 	//temporarily store the string between ()
 	//if find that... then search our global array for variable substition
@@ -18,5 +40,6 @@ expand_variables(char *line) {
 
 	//if all these fail, enter the empty string
 
+	//repeat all this for the possible next variable substitution
 	//return to the process_bakefile function 	
 }

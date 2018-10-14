4da2e3c9ceb7ef305d93a737f6ca1500a7e6b634
Alexander Frazis <alexanderfrazis@gmail.com>
2018-10-14 17:00:13 +0800

    expand variables addition

--- expand_variables.c
+++ expand_variables.c
@@ -7,26 +7,19 @@
 	How are variables identified?
 */
 
-//line is a non empty, no leading space sgring.
-void expand_variables (char * line, int * no_variables, char ** var_name_list, char ** var_value_list) {
 
-	int length = strlen(line);
-	for (int i = 0; i < length; ++i ) { //doesnt hit null byte
-		int var_name_length = - 1
-		char * var_name;
 
-		if (line[i] == '$') {
-			var_name_length = size_of_var(line, i); //iinclude %()
-			var_name = get_var_name(line, i, var_name_length);
-			var_value = get_var_value (var_name);
+//assume $() is a must
+//without braces its an error
+//Assume () only braces allowed
+//error if $() is empty
+//assume space int the brackets throws an error. $(var ) or $( var )
+//assume $ (var) throws an error
 
-			move_back (line, i, var_name_length, length);
-			line = insert_string (line, var_value, i);
-			length = strlen(line);
-		}
-	}
-	return line;
-}
+	//does $ define a variable
+	//does $(...) define a variable
+	//what if you see $word
+	//what if you see $ word
 
 	// 1. find the variables using the dollar sign.
 	// 2. store variable names
@@ -38,8 +31,76 @@ void expand_variables (char * line, int * no_variables, char ** var_name_list, c
 	//can you use other brackets
 	//can you use wildcards
 
+//line is a non empty, no leading space string.
+void expand_variables (char * line, int * no_variables, char ** var_name_list, char ** var_value_list) {
 
+	int length = strlen(line);
+	for (int i = 0; i < length; ++i ) { //doesnt hit null byte
+		int var_name_length = - 1
+		char * var_name;
 
-int size_of_var (char * line, int var_start_po) {
+		if (line[i] == '$') {
+			var_name_length = get_var_length (line, i , length)
+			var_name = get_var_name(line, i, var_name_length);
+			var_value = get_var_value (var_name, var_naem_list, var_value_list);
+			
+
+			move_back (line, i, var_name_length + 3, length); //$() 3 extra characters
+			line = insert_string(line, var_value, i);
+			
+			length = strlen(line);
+
+			free(var_name);
+			free (var_value);
+		}
+	}
+	return line;
+}
+
+//Gets the var length and validates the variable
+int get_var_length (char * line, int var_start, int length) {
+	int i = var_start;
+	if (length < i + 4) {
+		printf("need a minimum length four for a variable") //should go to an error
+		exit(EXIT_FAILURE);
+	}
+	if (line[i] != '$') {
+		printf("$ not at start of variable name") //should go to an error
+		exit(EXIT_FAILURE);
+	}
+	if (line [i + 1] != '(') {
+		printf("$ not followed by (") //should go to an error
+		exit(EXIT_FAILURE);
+	}
+	i = var_start + 2;
+	while (line[i] != ')') {
+		if (line[i] == '\0') {
+			printf("variable does not have close bracket )") //should go to an error
+			exit(EXIT_FAILURE);
+		}
+		if (line[i] == '\t' || line[i] == ' ') {
+			printf("spaces not allowed in variable reference") //should go to an error
+			exit(EXIT_FAILURE);
+		}
+		++i
+	}
+	//at this point line[i] = ')'
+	int var_length = i = var_start_po - 3;
+	return var_length
+}
+
+char * get_var_name(line *, int var_start, int var_name_length) {
+	char * var_name = calloc (var_name_length + 1, sizeof(char));
+	int i = var_start + 2;
+	for (i, i < i + var_name_length; ++i) {
+		var_name[i - var_start - 2] = line[i]; 
+	}
+	var_name[i] = '\0';
+	return var_name;
+
+}	
+
+char * get_var_value (char * var_name) {
 
 }
+	

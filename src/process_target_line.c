78030216136518ef7373609b9589bb1012c52585
JacobU <kieren.underwood@gmail.com>
2018-10-22 22:27:27 +0800

    first

--- src/process_target_line.c
+++ src/process_target_line.c
@@ -22,151 +22,155 @@
 
 // */
 
-// time_t get_url_time(char *url) {
-// 	//curl -s -v --head http://foo.com/bar/baz.pdf 2>&1 | grep '^< Last-Modified:'
-
-// 	int pid = fork();
-
-// }
-
-// int url_exists(char *url) {
-
-// 	CURL = *curl;
-// 	CURLcode isurl;
-
-// 	curl = curl_easy_init();
-
-// 	if(curl) {
-// 		curl_easy_setopt(curl, CURLOPT_URL, url);
-
-// 		//Dont send to stdout
-// 		curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
-
-// 		//Send request
-// 		isurl = curl_easy_perform(curl);
-// 		curl_easy_cleanup(curl);
-// 	}
-
-// 	return (isurl == CURLE_OK) ? 1 : 0;
-// }
-
-// bool startswith(char *url, char *dep) {
-
-// 	//Arbitrary function for checking start of strings
-// 	if(strncpm(url, dep, strlen(url) == 0)) {
-// 		return 1;
-// 	}
-
-// 	return 0; 
-// }		
-
-// bool check_if_url(char * dependency) {
-
-// 	//Check to see whether the dependency looks like a url
-// 	const char *url1 = "file://";
-// 	const char *url2 = "http://";
-// 	const char *url3 = "https://";
-
-// 	//If yes
-// 	if(startswith(url1, dependency) || startswith(url2, dependency) || startswith(url3, dependency)) {
+bool is_in_current_dir(char *targetname) {
 	
-// 		return 1;
+}
 
-// 	}
-// 	//If not a url
-// 	return 0;
+time_t get_url_time(char *url) {
+	//curl -s -v --head http://foo.com/bar/baz.pdf 2>&1 | grep '^< Last-Modified:'
 
-// }
+	int pid = fork();
 
-// process_this_line(char *target, char **dependencies, char *action_line) {
+}
 
-// 	int something++ = 0;
+int url_exists(char *url) {
 
-// 	while(there are still dependencies to examine, ie **dependencies != '\0') {
+	CURL = *curl;
+	CURLcode isurl;
 
-// 		if(dependency is itself a target--check the data structure) {
+	curl = curl_easy_init();
+
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+
+		//Dont send to stdout
+		curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+
+		//Send request
+		isurl = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+	}
+
+	return (isurl == CURLE_OK) ? 1 : 0;
+}
+
+bool startswith(char *url, char *dep) {
+
+	//Arbitrary function for checking start of strings
+	if(strncpm(url, dep, strlen(url) == 0)) {
+		return 1;
+	}
+
+	return 0; 
+}		
+
+bool check_if_url(char * dependency) {
+
+	//Check to see whether the dependency looks like a url
+	const char *url1 = "file://";
+	const char *url2 = "http://";
+	const char *url3 = "https://";
+
+	//If yes
+	if(startswith(url1, dependency) || startswith(url2, dependency) || startswith(url3, dependency)) {
+	
+		return 1;
+
+	}
+	//If not a url
+	return 0;
+
+}
+
+process_this_line(char *target, char **dependencies, char *action_line) {
+
+	int something++ = 0;
+
+	while(there are still dependencies to examine, ie **dependencies != '\0') {
+
+		if(dependency is itself a target--check the data structure) {
 				
-// 			(somehow access data from the structure)
+			(somehow access data from the structure)
 			
-// 			char *newtarget = struct.target;
-// 			char **newdependencies = struct.depenedencies;
-// 			char *newaction_line = struct.action_line;
-// 			process_this_line(newtarget, newdependencies, newaction_line);
+			char *newtarget = struct.target;
+			char **newdependencies = struct.depenedencies;
+			char *newaction_line = struct.action_line;
+			process_this_line(newtarget, newdependencies, newaction_line);
 
-// 		} else if(check_if_url(*dependencies)) {
+		} else if(check_if_url(*dependencies)) {
 			
-// 			DO URL-y THINGS HERE;
+			DO URL-y THINGS HERE;
 
-// 		} else if(dependency is a file that can be found) {
+		} else if(dependency is a file that can be found) {
 
-// 			move to next dependency;
+			move to next dependency;
 
-// 		} else if(dependency does not exist) {
+		} else if(dependency does not exist) {
 				
-// 			send some error;
-// 		}
-// 		something++;	
+			send some error;
+		}
+		something++;	
 
-// 	}	
+	}	
 
 		
-// 	if(target was modified more recently than all dependencies) {
+	if(target was modified more recently than all dependencies) {
 			
-// 		do nothing with this entire line--ie exit;
+		do nothing with this entire line--ie exit;
 		
-// 	} else {
+	} else {
 
-// 		run action_line on SHELL;
-// 	}	
+		run action_line on SHELL;
+	}	
 
-// }	
+}	
 
-// char ** isolate_dependencies(char *rest_of_line) {
-// 	//split up all dependencies and return an array of char pointers
-// }
+char ** isolate_dependencies(char *rest_of_line) {
+	//split up all dependencies and return an array of char pointers
+}
 
-// //If(no )
-
-
-// process_target_line(char *firstword, char *rest_of_line, int *no_variables, int *variable_length) {
-
-// 	create structure filled with (MAYBE PUT THIS OUTSIDE OF THIS FUNCTION) 
-// 	- target
-// 	- array of dependencies
-// 	- action line 
+//If(no )
 
 
-// 	//ALGORITHM FOR THIS FUNCTION
+process_target_line(char *firstword, char *rest_of_line, int *no_variables, int *variable_length) {
 
-// 	char **dependencies = isolate_dependencies(&rest_of_line);
+	create structure filled with (MAYBE PUT THIS OUTSIDE OF THIS FUNCTION) 
+	- target
+	- array of dependencies
+	- action line 
 
-// 	process_this_line(target, dependencies, action_line);
+
+	//ALGORITHM FOR THIS FUNCTION
+
+	char **dependencies = isolate_dependencies(&rest_of_line);
+
+	process_this_line(target, dependencies, action_line);
 	
 
-// 	//Find current directory
+	//Find current directory
 	
 
-// 	/*char *curr_dir = get_current_dir_name();
-// 	//need to call free() after, when using "get_current_dir_name()"
+	/*char *curr_dir = get_current_dir_name();
+	//need to call free() after, when using "get_current_dir_name()"
 
-// 	if(first_target_line) {
-// 		char *default_target = firstword;
-// 	}
+	if(first_target_line) {
+		char *default_target = firstword;
+	}
 
-// 	struct stat file
-// 	if(stat(firstword, )) 
+	struct stat file
+	if(stat(firstword, )) 
 	
 
-// 	//we need to chop off the ":"
-// 	//then we need to get the next word
-// 	//then we find the latest modification date of this word (possibly using stat())
-// 	//compare this with the modification date of the *firstword
-// 	//if *firstword is earlier, then 
+	//we need to chop off the ":"
+	//then we need to get the next word
+	//then we find the latest modification date of this word (possibly using stat())
+	//compare this with the modification date of the *firstword
+	//if *firstword is earlier, then 
 
 
-// 	if(there are no dependencies) {
-// 		rebuild the target
-// 	}
+	if(there are no dependencies) {
+		rebuild the target
+	}
 
-// 	*/
-//}
+	*/
+}

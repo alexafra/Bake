7ae7bd9bf8b8805d793caf8247ff50e4e0934e44
Alexander Frazis <alexanderfrazis@gmail.com>
2018-10-24 21:09:01 +0800

    nearly there baybe

--- src/bake.c
+++ src/bake.c
@@ -4,6 +4,29 @@
 
 #endif
 
+void print_bakefile (void) {
+    int i = 0;
+    printf("Targets:\n\n");
+    while (targets[i] != NULL) {
+        printf("\tTarget:\n");
+        printf("\t\t%s\n", targets[i]->target);
+        int j = 0;
+        printf("\tDependencies:\n");
+        while (targets[i]->dependencies[j] != NULL) {
+            printf("\t\t%s\n", targets[i]->dependencies[j]);
+            ++j;
+        }
+        int k = 0;
+        printf("\tActions:\n");
+        while (targets[i]->actions[k] != NULL) {
+            printf("\t\t%s\n", targets[i]->actions[k]);
+            ++k;
+        }
+        ++i;
+        printf("\n");
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -16,16 +39,17 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        variables = (Variable **) calloc (1, sizeof(Variable*));
-        *variables = NULL;
-
         targets = (Target **) calloc (1, sizeof(Target*));
         *targets = NULL;
         
         process_bakefile(fp);
                              // WE OPENED IT, SO WE CLOSE IT
+        print_bakefile();
         fclose(fp);
     }
 
     return 0;
 }
+
+
+
